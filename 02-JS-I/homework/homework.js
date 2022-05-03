e2c3b0589a3beff35448d34d2921c226b0f376d8
unknown <agustineliasbustos@gmail.com>
2022-05-02 23:06:18 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'agus';
 const nuevoNum = "42";
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = sun;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - null === 5;
